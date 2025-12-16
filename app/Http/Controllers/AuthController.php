41aafd83340287cae83099e9b5d893181fe39f8a
Mahdi <rahnamamahdi78@gmail.com>
2025-12-16 22:18:15 +0330

    create register process

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserRegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -24,6 +26,16 @@ class AuthController extends Controller
     public function registerForm()
     {
         return view('auth.register');
+    }
+
+    public function register(UserRegisterRequest $request)
+    {
+        $user=User::create([
+            ...$request->validated(),
+            'height'=>($request->height)/100
+        ]);
+        Auth::login($user);
+        return to_route('home');
     }
     public function logout(){
         Auth::logout();
